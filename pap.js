75a18079792a290996714423fadea0867ba7b531
NaniMadhu94 <102646505+NaniMadhu94@users.noreply.github.com>
2022-04-06 10:27:16 +0530

    Update pap.js

--- pap.js
+++ pap.js
@@ -1,9 +1,9 @@
 
 var firstPromise = first();
 
+/* chain the promises together using then */
 var secondPromise = firstPromise.then(function (val) {
   return second(val);
 });
 
 secondPromise.then(console.log);
-

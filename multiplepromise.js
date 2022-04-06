47f19495dba4580cacc0c59775c597c315eaa17c
NaniMadhu94 <102646505+NaniMadhu94@users.noreply.github.com>
2022-04-06 10:25:21 +0530

    Update multiplepromise.js

--- multiplepromise.js
+++ multiplepromise.js
@@ -1,8 +1,11 @@
+
+/* crwate a function aa that accepts 2 promises as argvs*/
 function all(a, b) {
   return new Promise(function (fulfill, reject) {
     var counter = 0;
     var out = [];
-
+/* Attach then fulfillment handlers to both promises and increment the internal
+counter when the handlers are called.*/
     a.then(function (val) {
       out[0] = val;
       counter++;

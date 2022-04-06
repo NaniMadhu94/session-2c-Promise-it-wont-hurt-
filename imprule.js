809d3211b847753bd6b1d5d5935e41bc6aa4f4c7
NaniMadhu94 <102646505+NaniMadhu94@users.noreply.github.com>
2022-04-06 10:24:43 +0530

    Update imprule.js

--- imprule.js
+++ imprule.js
@@ -1,16 +1,22 @@
+/*  Create a function `iterate` that prints the first argument(an integer)
+ to it and then returns that argument + 1;*/
+
 function iterate(num) {
   console.log(num);
   return num + 1;
 }
 
+/*  Create a function `alwaysThrows` that throws an `Error` withtext `"OH NOES"`
+ */
 function alwaysThrows() {
   throw new Error('OH NOES');
 }
 
+/* attach a rejection handler */
 function onReject(error) {
   console.log(error.message);
 }
-
+/* insert a call to 'alwaysThrows' after the 5th call*/
 Promise.resolve(iterate(1))
 .then(iterate)
 .then(iterate)
